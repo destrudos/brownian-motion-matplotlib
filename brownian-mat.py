b3c2fd7acc5cdff74c50b8c5b020bb03d03cfaa0
destrudos <destrudos@protonmail.com>
2023-04-11 00:27:01 +0200

    Added script with rectangular and rotating particles

--- brownian-mat.py
+++ brownian-mat.py
@@ -1,41 +1,46 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.patches import Rectangle
 
 # Define the simulation parameters
 n_particles = 13
 n_steps = 1000
 vessel_width = 150
 vessel_height = 150
-particle_radius = 2
+particle_size = 4
 dt = 0.1
 
-# Set up the initial particle positions and velocities
-positions = np.random.rand(n_particles, 2) * (vessel_width - 2 * particle_radius) + particle_radius
+# Set up the initial particle positions, velocities, and angles
+positions = np.random.rand(n_particles, 2) * (vessel_width - 2 * particle_size) + particle_size
 velocities = np.zeros((n_particles, 2))
+angles = np.random.rand(n_particles) * 360
 
-# Define a function to update the particle positions
-def update_positions(positions, velocities, dt):
+# Define a function to update the particle positions and angles
+def update_positions(positions, velocities, angles, dt):
     # Add a random displacement to each particle based on the Brownian motion model
     displacements = np.random.normal(loc=0, scale=np.sqrt(2*dt), size=(n_particles, 2))
     positions += displacements
 
     # Check for collisions with the walls of the vessel
-    positions = np.where(positions < particle_radius, 2 * particle_radius - positions, positions)
-    positions = np.where(positions > vessel_width - particle_radius, 2 * (vessel_width - particle_radius) - positions, positions)
-    positions = np.where(positions < particle_radius, 2 * particle_radius - positions, positions)
-    positions = np.where(positions > vessel_height - particle_radius, 2 * (vessel_height - particle_radius) - positions, positions)
+    positions = np.where(positions < particle_size, 2 * particle_size - positions, positions)
+    positions = np.where(positions > vessel_width - particle_size, 2 * (vessel_width - particle_size) - positions, positions)
+    positions = np.where(positions < particle_size, 2 * particle_size - positions, positions)
+    positions = np.where(positions > vessel_height - particle_size, 2 * (vessel_height - particle_size) - positions, positions)
 
     # Check for collisions between particles
     for i in range(n_particles):
         for j in range(i+1, n_particles):
             dist = np.linalg.norm(positions[i] - positions[j])
-            if dist < 2 * particle_radius:
-                overlap = 2 * particle_radius - dist
+            if dist < 2 * particle_size:
+                overlap = 2 * particle_size - dist
                 direction = (positions[i] - positions[j]) / dist
                 positions[i] += overlap / 2 * direction
                 positions[j] -= overlap / 2 * direction
 
-    return positions
+    # Add a random rotation to each particle around its center
+    angles += np.random.normal(loc=0, scale=5, size=n_particles)
+
+    return positions, angles
 
 # Create a plot to display the simulation results
 fig, ax = plt.subplots()
@@ -45,19 +50,20 @@ ax.set_ylim(0, vessel_height)
 # Generate random colors for the particles
 colors = np.random.rand(n_particles, 3)
 
-# Create a list of particle circles with random colors
-particles = [plt.Circle((0, 0), particle_radius, color=colors[i]) for i in range(n_particles)]
+# Create a list of particle rectangles with random colors and angles
+particles = [Rectangle((0, 0), particle_size, particle_size, angle=angles[i], color=colors[i]) for i in range(n_particles)]
 
-# Add the particle circles to the plot
+# Add the particle rectangles to the plot
 for p in particles:
     ax.add_artist(p)
 
-# Define the animation function to update the plot with new particle positions
+# Define the animation function to update the plot with new particle positions and angles
 def animate(frame):
-    global positions, velocities
-    positions = update_positions(positions, velocities, dt)
+    global positions, velocities, angles
+    positions, angles = update_positions(positions, velocities, angles, dt)
     for i, p in enumerate(particles):
-        p.center = positions[i]
+        p.set_xy(positions[i] - particle_size / 2)
+        p.set_angle(angles[i])
     return particles
 
 # Animate the plot
